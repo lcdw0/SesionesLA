abc30b17dc807c591e048b81e2735a956f4b1783
Mario <mjcarrion@uamv.edu.ni>
2024-04-12 07:35:31 -0600

    Ejercicio de arreglo numeros pares

---
+++ Ejercicio1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int num1;
+    float num2;
+    double num3;
+    char letra;
+    char frase[30];
+    system("color a5");
+    printf("Bienvenido a mi ejercicio 1 \n");
+    printf("Leer un numero: ");
+    scanf("%i", &num1);
+    printf("%i se guardo correctamente \n", num1);
+    system("pause");
+    system("cls||clear");
+    printf("Leer un flotante: ");
+    scanf("%f", &num2);
+    printf("Guarde un flotante: %.2f", num2);
+    
+    return 0;
+}
